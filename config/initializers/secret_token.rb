86ff7d33a4c2da0ec13ce3b9b4edac6026811096
Meng Hu <supermenghu@gmail.com>
2014-01-14 22:09:22 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloudtopFront::Application.config.secret_key_base = '92630c64110c8a33f6673c5b564ba8e9a29585147f2af6f3c9072183a79f4454a9d19d865ff10786eab58fd79fe0787d68c959410242108ab127ff4949cdad2f'
