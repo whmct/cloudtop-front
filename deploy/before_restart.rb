591c504e0827de658979e425cb527062af7dd3df
Meng Hu <supermenghu@gmail.com>
2014-01-16 09:11:32 -0800

    change system to run

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 
-system "sudo su cloudtop"
-system "echo $USER >> /opt/cloudtop/debug"
-system "ruby --version > /opt/cloudtop/ruby_v"
-system "cd /opt/cloudtop/web/current && bundle install"
-system "/opt/cloudtop/web/current/bin/rails server &"
-system "sudo su"
+run "echo $USER >> /opt/cloudtop/debug"
+run "ruby --version > /opt/cloudtop/ruby_v"
+run "cd /opt/cloudtop/web/current && bundle install"
+run "/opt/cloudtop/web/current/bin/rails server &"
