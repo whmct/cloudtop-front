4d76546bbfe7f1ab0ac829164447b1bc3d2ad990
Meng Hu <supermenghu@gmail.com>
2014-01-16 09:02:12 -0800

    some debug info

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
-system "echo $USER"
-system "source ~/.bash_profile"
+system "echo $USER >> /opt/cloudtop/debug"
 system "ruby --version > /opt/cloudtop/ruby_v"
 system "cd /opt/cloudtop/web/current && bundle install"
 system "/opt/cloudtop/web/current/bin/rails server &"
