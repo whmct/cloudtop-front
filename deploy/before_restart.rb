466ae574c152652757248fcc91b1a4c85829a73e
Meng Hu <supermenghu@gmail.com>
2014-01-16 23:38:44 -0800

    with the most confidence, let's try it again....

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,6 +1,3 @@
 
 # we should bundle install
-system "env > /tmp/env"
-system "which ruby > /tmp/ruby_where"
-system "rvm list > /tmp/rvm_list"
-system "cd #{release_path};bundle install"
+run "sudo bash --login -c 'cd #{release_path}; bundle install'"
